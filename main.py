1c5735c90a4a24f65805c70fcd017820321b362c
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2020-11-19 13:18:50 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ tempf=input.temperature(TemperatureUnit.FAHRENHEIT)
 
 while True:  
     print("temperature " + tempf)  
-    if tempf > 60:
+    if tempf >= 70:
         light.set_pixel_color(5, light.rgb(255, 0, 0))
         
+    elif 70>tempf>=40:
+        light.set_pixel_color(5, light.rgb(0, 255, 0))
+
     
     else:
-        light.set_pixel_color(5, light.rgb(0, 255, 0))
+        light.set_pixel_color(5, light.rgb(0, 0, 255))
--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@ let tempf = input.temperature(TemperatureUnit.Fahrenheit)
 // print("Current room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) +"°F - "  + input.temperature(TemperatureUnit.CELSIUS) + "°C")
 while (true) {
     console.log("temperature " + tempf)
-    if (tempf > 60) {
+    if (tempf >= 70) {
         light.setPixelColor(5, light.rgb(255, 0, 0))
-    } else {
+    } else if (70 > tempf && tempf >= 40) {
         light.setPixelColor(5, light.rgb(0, 255, 0))
+    } else {
+        light.setPixelColor(5, light.rgb(0, 0, 255))
     }
     
 }
