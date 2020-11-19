d7d0ab86c788f455f407a10effd54c847f05afeb
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2020-11-19 13:11:31 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-temp=input.temperature(TemperatureUnit.FAHRENHEIT)
-
+tempf=input.temperature(TemperatureUnit.FAHRENHEIT)
+#Tempf=60
 
 #while True:
     #print("Current room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) +"°F - "  + input.temperature(TemperatureUnit.CELSIUS) + "°C")
 
 
-
+while True:  
+    print("temperature " + tempf)  
+    if tempf > 60:
+        light.set_pixel_color(5, light.rgb(255, 0, 0))
+        
     
-if temp > 60:
-    light.set_pixel_color(5, light.rgb(255, 0, 0))
+    else:
+        light.set_pixel_color(5, light.rgb(0, 255, 0))
--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-let temp = input.temperature(TemperatureUnit.Fahrenheit)
+let tempf = input.temperature(TemperatureUnit.Fahrenheit)
+// Tempf=60
 // while True:
 // print("Current room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) +"°F - "  + input.temperature(TemperatureUnit.CELSIUS) + "°C")
-if (temp > 60) {
-    light.setPixelColor(5, light.rgb(255, 0, 0))
+while (true) {
+    console.log("temperature " + tempf)
+    if (tempf > 60) {
+        light.setPixelColor(5, light.rgb(255, 0, 0))
+    } else {
+        light.setPixelColor(5, light.rgb(0, 255, 0))
+    }
+    
 }
-
