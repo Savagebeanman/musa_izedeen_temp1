dda3401a32a5bc0ced9e8fb59420ea08cf89f4f8
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2020-11-24 09:52:32 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-tempf=input.temperature(TemperatureUnit.FAHRENHEIT)
-Tempf=30
+
 
 #while True:
     #print("Current room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) +"°F - "  + input.temperature(TemperatureUnit.CELSIUS) + "°C")
 
 
 while True:  
-    print("temperature " + Tempf)  
-    if Tempf >= 70:
+    print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT))  
+    if input.temperature(TemperatureUnit.FAHRENHEIT) >= 60:
         light.set_pixel_color(5, light.rgb(255, 0, 0))
         
-    elif 70>Tempf>=40:
+    elif 70>input.temperature(TemperatureUnit.FAHRENHEIT)>=40:
         light.set_pixel_color(5, light.rgb(0, 255, 0))
 
     else:
--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
-let tempf = input.temperature(TemperatureUnit.Fahrenheit)
-let Tempf = 30
 // while True:
 // print("Current room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) +"°F - "  + input.temperature(TemperatureUnit.CELSIUS) + "°C")
 while (true) {
-    console.log("temperature " + Tempf)
-    if (Tempf >= 70) {
+    console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit))
+    if (input.temperature(TemperatureUnit.Fahrenheit) >= 60) {
         light.setPixelColor(5, light.rgb(255, 0, 0))
-    } else if (70 > Tempf && Tempf >= 40) {
+    } else if (70 > input.temperature(TemperatureUnit.Fahrenheit) && input.temperature(TemperatureUnit.Fahrenheit) >= 40) {
         light.setPixelColor(5, light.rgb(0, 255, 0))
     } else {
         light.setPixelColor(5, light.rgb(0, 0, 255))
